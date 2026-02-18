fb29f62a71f35eacfb5af97260969909b3af624b
Md. Tanver Rahman <trshoccho@gmail.com>
2026-02-18 14:38:55 +0600

    fixing markdown preview

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,5 @@
+-- In your markview setup
+
 
 vim.opt_local.makeprg = 'pandoc "%" -o "%:r.pdf" --css "style.css" --pdf-engine wkhtmltopdf'
 -- Toggle Markdown Preview
--- lua/so/lazy/extra.lua
+++ lua/so/lazy/extra.lua
@@ -1,10 +1,26 @@
 return {
-  { "OXY2DEV/markview.nvim",    lazy = false },
   {
-    "iamcco/markdown-preview.nvim",
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    "OXY2DEV/markview.nvim", lazy = false,
+  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = "cd app && yarn install",
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { "markdown" }
+  --   end,
+  --   ft = { "markdown" },
+  -- },
+  {
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    config = function()
+      require("markdown_preview").setup({
+        -- all optional; sane defaults shown
+        port = 8421,
+        open_browser = true,
+        debounce_ms = 300,
+      })
     end,
   },
   { "folke/todo-comments.nvim", opts = {} },
