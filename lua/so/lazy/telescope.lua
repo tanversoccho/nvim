5779c4e6c2b23152b840302e6474e2fc038c4a49
MD. Tanver Rahman Soccho <trshoccho@gmail.com>
2026-03-29 22:51:03 +0600

    add trouble

--- lua/so/lazy/telescope.lua
+++ lua/so/lazy/telescope.lua
@@ -59,42 +59,47 @@ return {
             require('telescope.themes').get_dropdown(),
           },
           -- zoxide = {
-          --   -- your config here
-          -- }
-        },
-      }
-
-      -- require('telescope').load_extension( 'fzf')
-      -- require('telescope').load_extension( 'ui-select')
-      require('telescope').load_extension('luasnip')
-      -- require("telescope").load_extension("zoxide")
-
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader><leader>', builtin.builtin, { desc = 'Telescope' })
-      vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = '[F]iles' })
-      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[K]eymaps' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[H]elp' })
-      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Grep [W]ord' })
-      vim.keymap.set('n', '<leader>rg', builtin.live_grep, { desc = 'Live [G]rep' })
-      vim.keymap.set('n', '<leader>fq', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>ff', builtin.resume, { desc = 'telescope [R]esume' })
-      vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'Recent Files' })
-      vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'buffers' })
-      vim.keymap.set('n', "'", builtin.marks, { desc = 'Marks' })
-      vim.keymap.set('n', '<C-s>', builtin.registers, { desc = 'Telescope: Show registers' })
-      vim.keymap.set('n', '<leader>fs', function()
-        require('telescope').extensions.luasnip.luasnip()
-      end, { desc = '[F]ind [S]nippets' })
-
-      vim.keymap.set('n', '<leader>f/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
+            --   -- your config here
+            -- }
+          },
         }
-      end, { desc = '[/] in Open Files' })
 
-      vim.keymap.set('n', '<leader>fn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[N]vim config files' })
-    end,
-  }
+        -- require('telescope').load_extension( 'fzf')
+        -- require('telescope').load_extension( 'ui-select')
+        require('telescope').load_extension('luasnip')
+        -- require("telescope").load_extension("zoxide")
+
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader><leader>', builtin.builtin, { desc = 'Telescope' })
+        vim.keymap.set('n', '<leader>fd', builtin.find_files, { desc = '[F]iles' })
+        vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[K]eymaps' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[H]elp' })
+        vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Grep [W]ord' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live [G]rep' })
+        vim.keymap.set('n', '<leader>fq', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>ff', builtin.resume, { desc = 'telescope [R]esume' })
+        vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'Recent Files' })
+        vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'buffers' })
+        -- Jumplist
+        vim.keymap.set('n', '<leader>j', builtin.jumplist, { desc = 'Jumplist' })
+        -- Changelist
+        -- vim.keymap.set('n', '<leader>c', builtin.changelist, { desc = 'Changelist' })
+        -- Registers
+        vim.keymap.set('n', '<leader>r', builtin.registers, { desc = 'Registers' })
+        vim.keymap.set('n', "'", builtin.marks, { desc = 'Marks' })
+        vim.keymap.set('n', '<leader>fs', function()
+          require('telescope').extensions.luasnip.luasnip()
+        end, { desc = '[F]ind [S]nippets' })
+
+        vim.keymap.set('n', '<leader>f/', function()
+          builtin.live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files',
+          }
+        end, { desc = '[/] in Open Files' })
+
+        vim.keymap.set('n', '<leader>fn', function()
+          builtin.find_files { cwd = vim.fn.stdpath 'config' }
+        end, { desc = '[N]vim config files' })
+      end,
+    }
