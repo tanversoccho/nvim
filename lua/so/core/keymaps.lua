d91fa74f370f7dbc04dd383fd72b8838683ba648
MD. Tanver Rahman Soccho <trshoccho@gmail.com>
2026-03-30 20:29:13 +0600

    fixing

--- lua/so/core/keymaps.lua
+++ lua/so/core/keymaps.lua
@@ -1,25 +1,25 @@
+local map = vim.keymap
+-- map.set("n", "<space>x", ":.lua<CR>")
+-- map.set("v", "<space>x", ":lua<CR>")
+map.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map.set("n", "<leader>x", "<cmd>source %<CR>")
 
--- vim.keymap.set("n", "<space>x", ":.lua<CR>")
--- vim.keymap.set("v", "<space>x", ":lua<CR>")
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set("n", "<leader>x", "<cmd>source %<CR>")
 
+map.set('n', '<leader>i', '<cmd>InspectTree<CR>')
 
-vim.keymap.set('n', '<leader>i', '<cmd>InspectTree<CR>')
-
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>")
+map.set("n", "<leader>e", "<cmd>Neotree toggle<CR>")
 
 -- Swap : and ; in Normal mode
-vim.keymap.set({'v','n'}, ':', ';', { desc = 'Enter command line' })
-vim.keymap.set({'v','n'}, ';', ':', { desc = 'Repeat f/t motion' })
+map.set({'v','n'}, ':', ';', { desc = 'Enter command line' })
+map.set({'v','n'}, ';', ':', { desc = 'Repeat f/t motion' })
 
-vim.keymap.set("n", "\\=", function()
+map.set("n", "\\=", function()
   local view = vim.fn.winsaveview()
   vim.cmd("normal! gg=G")
   vim.fn.winrestview(view)
 end, { desc = "Reindent whole file (keep cursor)" })
 
-vim.keymap.set('n', '<leader>q', function()
+map.set('n', '<leader>q', function()
   for _, win in ipairs(vim.fn.getwininfo()) do
     if win.loclist == 1 then
       vim.cmd('lclose')
@@ -29,7 +29,7 @@ vim.keymap.set('n', '<leader>q', function()
   vim.diagnostic.setloclist()
 end, { desc = 'Toggle diagnostic location list' })
 
-vim.keymap.set('n', '<leader>Q', function()
+map.set('n', '<leader>Q', function()
   for _, win in ipairs(vim.fn.getwininfo()) do
     if win.quickfix == 1 then
       vim.cmd('cclose')
@@ -41,23 +41,85 @@ vim.keymap.set('n', '<leader>Q', function()
 end, { desc = 'Toggle diagnostic quickfix list' })
 
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Move Lines
-vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+map.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 -- Move Lines
-vim.keymap.set("n", "<A-down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-vim.keymap.set("n", "<A-up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-vim.keymap.set("i", "<A-down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<A-up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-vim.keymap.set("v", "<A-down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-vim.keymap.set("v", "<A-up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+map.set("n", "<A-down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map.set("n", "<A-up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map.set("i", "<A-down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map.set("i", "<A-up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map.set("v", "<A-down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map.set("v", "<A-up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+-- map.set("n", "<space>x", ":.lua<CR>")
+-- map.set("v", "<space>x", ":lua<CR>")
+map.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map.set("n", "<leader>x", "<cmd>source %<CR>")
+
+
+map.set('n', '<leader>i', '<cmd>InspectTree<CR>')
+
+map.set("n", "<leader>e", "<cmd>Neotree toggle<CR>")
+
+-- Swap : and ; in Normal mode
+map.set({'v','n'}, ':', ';', { desc = 'Enter command line' })
+map.set({'v','n'}, ';', ':', { desc = 'Repeat f/t motion' })
+
+map.set("n", "\\=", function()
+  local view = vim.fn.winsaveview()
+  vim.cmd("normal! gg=G")
+  vim.fn.winrestview(view)
+end, { desc = "Reindent whole file (keep cursor)" })
+
+map.set('n', '<leader>q', function()
+  for _, win in ipairs(vim.fn.getwininfo()) do
+    if win.loclist == 1 then
+      vim.cmd('lclose')
+      return
+    end
+  end
+  vim.diagnostic.setloclist()
+end, { desc = 'Toggle diagnostic location list' })
+
+map.set('n', '<leader>Q', function()
+  for _, win in ipairs(vim.fn.getwininfo()) do
+    if win.quickfix == 1 then
+      vim.cmd('cclose')
+      return
+    end
+  end
+  vim.diagnostic.setqflist()
+  vim.cmd('copen')
+end, { desc = 'Toggle diagnostic quickfix list' })
+
+
+map.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Move Lines
+map.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+-- Move Lines
+map.set("n", "<A-down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map.set("n", "<A-up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map.set("i", "<A-down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map.set("i", "<A-up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map.set("v", "<A-down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map.set("v", "<A-up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
