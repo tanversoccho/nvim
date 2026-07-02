9c5054f5eb605938fb1b18388c97ba8b232a6286
MD. Tanver Rahman Soccho <trshoccho@gmail.com>
2026-07-02 13:12:11 +0600

    adding windows
    c
    
    clear

--- lua/so/windows.lua
+++ lua/so/windows.lua
@@ -1,7 +1,9 @@
-vim.o.shell = "pwsh.exe"
-vim.o.shellcmdflag = "-ExecutionPolicy Bypass -NoProfile -c"
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
-
--- Add this line to make the terminal ignore the formatting junk
-vim.opt.shellredir = '>%s 2>&1'
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+    vim.o.shell = "pwsh.exe"
+    vim.o.shellcmdflag = "-NoProfile -c"
+    vim.o.shellquote = ""
+    vim.o.shellxquote = ""
+    vim.opt.shellredir = '>%s 2>&1'
+    -- Disable ANSI colors
+    vim.env.TERM = "dumb"
+end
